39fe8ddf63679f475fc111891299fca31afe1f8f
ebruhns <elliebruhns@gmail.com>
2021-06-04 02:38:52 -0700

    fixed menu and added songs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -22,6 +22,26 @@ public class MainMenu : MonoBehaviour
 		SceneManager.LoadScene("YDB");
 	}
 
+	public void PlayAllYouNeed()
+	{
+		SceneManager.LoadScene("AllYouNeed");
+	}
+
+	public void PlayhotelCali()
+	{
+		SceneManager.LoadScene("hotelCali");
+	}
+
+	public void PlayStairway()
+	{
+		SceneManager.LoadScene("stairway2Heaven");
+	}
+
+	public void PlayWeWillRockYou()
+	{
+		SceneManager.LoadScene("WeWillRockYou");
+	}
+
 	public void QuitGame()
 	{
 		Application.Quit();
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI; 
+
+    public GameObject optionsMenuUI;
+
+    // Update is called once per frame
+   public void Onpress_escape(InputAction.CallbackContext context)
+   {
+  		if (GameIsPaused)
+  			{
+  				Resume();
+  			}
+  			else
+  			{
+  				Pause();
+  			}
+    }      
+    
+
+    public void Resume()
+    {
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    	//make countdown happen
+    	Time.timeScale = 1f;
+    	GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+    	SceneManager.LoadScene("PauseMenu");
+    	Time.timeScale = 0f;
+    	GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OptionsMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+
+    }
+
+    public void QuitGame()
+    {
+      Application.Quit();
+    }
+}
